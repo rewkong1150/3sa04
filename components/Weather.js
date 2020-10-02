59036d21f6d7f3487b92dbfa8931aed0b2d875c7
rewkong1150 <chob-nutna@hotmail.com>
2020-10-02 18:00:17 +0700

    commit1

---
+++ components/Weather.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import Forecast from './Forecast';
+
+export default function Weather(props) {
+
+    const apiKey = 'e94a4ddccbcae552028f7b5214a238ff'
+
+    const [forecastInfo, setForecastInfo] = useState({
+        main: 'main',
+        description: 'description',
+        temp: 0,
+    })
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {   
+
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=${apiKey}`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                    console.log(forecastInfo)
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
+
+
+
+    return (
+        <View>
+            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                <View style={styles.cover}>
+                    <Text style={styles.medium}>Zip Code: {props.zipCode}</Text>
+                    <Forecast {...forecastInfo} />
+                </View>
+            </ImageBackground>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+    },
+    cover: {
+        backgroundColor: '#000',
+        width: '100%',
+        height: 300,
+        opacity: 0.5,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    medium: {
+        fontSize: 20,
+        color: '#FFF',
+    }
+});
---
+++ components/ZipCodeScreen.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, FlatList, TouchableHighlight, StyleSheet} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+
+const availableZipItems = [
+    { place: 'Hatyai', code: '90110' },
+    { place: 'Trang', code: '92000' },
+    { place: 'Chiangmai', code: '50000' },
+    { place: 'Khonkaen', code: '40000' },
+    { place: 'Chonburi', code: '20000' },
+    { place: 'Phuket', code: '83120'},
+]
+
+const ZipItem = ({ place, code, navigation }) => (
+    <TouchableHighlight onPress={() => navigation.navigate('Weather', { zipCode: code })}>
+        <View>
+            <Text>{place}</Text>
+            <Text>{code}</Text>
+        </View>
+    </TouchableHighlight>
+)
+
+
+const _keyExtractor = item => item.code
+
+export default function ZipCodeScreen() {
+    const navigation = useNavigation()
+    return (
+        <View style={styles.center}>
+            
+            <FlatList
+                data={availableZipItems}
+                keyExtractor={_keyExtractor}
+                renderItem={({ item }) => <ZipItem {...item} navigation={navigation} />}
+            />
+            <StatusBar style="auto" />
+        </View>
+    );
+
+}
+const styles = StyleSheet.create({
+    center: {
+        alignItems: 'center',
+        fontSize: 30,
+        color: '#FFF',
+        marginTop: 20,
+    },
+     
+})
